b568d48576f64643c97e9edd5bf1a3ad10cdc5ca
montytecnologia <lvilla@mgpsa.com>
2020-06-19 14:25:23 +0200

    añadido bean tracerApi y clase TracerCustom que implementa y extienda la interfaz y la implementación de tracer (chapu) TracerImpl debería implementar TracerAPI

---
+++ src/main/java/org/karabacode/calculadora/util/TracerCustom.java
@@ -0,0 +1,7 @@
+package org.karabacode.calculadora;
+
+import io.corp.calculator.TracerAPI;
+import io.corp.calculator.TracerImpl;
+
+public class TracerCustom extends TracerImpl implements TracerAPI  {
+}
